c77e0addd0ed5d42a4727d395519258c3f15a7ea
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-06-20 11:46:05 -0700

    more formating

--- main.js
+++ main.js
@@ -1,10 +1,43 @@
-console.log('test test test')
+// Random Button Logic
+function randomNum(num) { 
+    let random = Math.trunc(Math.random() * num);
+    return random;
+}
 
-axios.get('https://pokeapi.co/api/v2/pokemon/ditto').then((response)=>{
-    typeUrl = response.data.types[0].type.url
-    return axios.get(typeUrl)
-}).then((typeData)=>{
-    console.log(typeData)
-}).catch((error)=>{
-    console.log('no good: ', error)
-})
+let pokeID = randomNum(898)
+// console.log(pokeID)
+// returns a number between 1-898
+const pokeTeam = [];
+let randomPoke = await axios.get('https://pokeapi.co/api/v2/pokemon/'+pokeID)
+pokeTeam.push(randomPoke.data.name)
+console.log(pokeTeam)
+
+
+
+
+
+
+
+
+
+
+
+
+// list in order of buttons
+const types = ["fire", "water", "electric", "grass", "ice", "ground", "flying", "ghost", "rock", "fighting", "poison", "psychic", "bug", "dark", "steel", "dragon", "fairy"]
+// each button will just be a search using types[]
+
+
+
+
+
+
+
+
+
+
+
+
+// -====----=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// randomType = types[Math.trunc(Math.random() * types.length)];
+// console.log(randomType)
