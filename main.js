e66708d85ecbe5b347e10329ccf13b9da5ee0600
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-06-21 19:39:51 -0700

    simplified page

--- main.js
+++ main.js
@@ -9,6 +9,8 @@
 // returns a number between 1-898
 // --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
+const { default: axios } = require("axios");
+
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // Each elemental Type button
 // const types = ["fire", "water", "electric", "grass", "ice", "ground", "flying", "ghost", "rock", "fighting", "poison", "psychic", "bug", "dark", "steel", "dragon", "fairy", "normal"]
@@ -16,28 +18,24 @@
 // console.log(randomType)
 // // returns each type
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-// const getMon = async () => {
-//     const pokemonCaught = []
-//     const pokemonURL = 'https://pokeapi.co/api/v2/pokemon'
-//     const typesURL = 'https://pokeapi.co/api/v2/type/'
-
-
-
 async function generatePokemon() {
     // const rand = Math.floor(Math.random() * (151 - 1) + 1);
-    const rand = Math.trunc(Math.random() * 898)
+    const rand = getRandomInt(151);
     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${rand}/`);
     const typeUrl = response.data.sprites.front_default;
     const pokeType = response.data.types[0].type.url;
 
     let img = document.createElement('img');
     img.src = typeUrl;
-    document.getElementById("pokeball").appendChild(img);
+    document.getElementById("sec").appendChild(img);
 
     for (let i = 0; i<5; i++) {
         await getPokeFromType(pokeType);
     }
+}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 async function getPokeFromType(typePage) {
     const response = await axios.get(typePage);
@@ -47,39 +45,3 @@ async function getPokeFromType(typePage) {
     img.src = pokeData.data.sprites.front_default;
     document.getElementById("sec").appendChild(img);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const pokeTeam = [];
-// let randomPoke = await axios.get('https://pokeapi.co/api/v2/pokemon/'+pokeID)
-// pokeTeam.push(randomPoke.data.name)
-// console.log(pokeTeam)
-
-
-// const getMon = async ()=>{
-//         const response = await axios.get("https://pokeapi.co/api/v2/pokemon/"+pokeID)
-//     	const typeUrl = response.data.types[0].type.url
-//     	console.log(typeUrl)
-//         const typeResponse = await axios.get(typeUrl)
-//         console.log(typeResponse)
-//     }
-    
-// onclick="getMon()"
-
-
-// list in order of buttons
-// const types = ["fire", "water", "electric", "grass", "ice", "ground", "flying", "ghost", "rock", "fighting", "poison", "psychic", "bug", "dark", "steel", "dragon", "fairy", "normal"]
-// each button will just be a search using types[]
-
