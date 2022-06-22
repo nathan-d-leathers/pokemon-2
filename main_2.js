69b2a1e6cdb37567bc2f242561eed5d0656613f6
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-06-22 15:37:35 -0700

    working random button

--- main_2.js
+++ main_2.js
@@ -1,27 +1,24 @@
-// --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-// Random Button Logic
-// function randomNum(num) { 
-//     let random = Math.trunc(Math.random() * num);
-//     return random;
-// }
-// let pokeID = randomNum(898)
-// console.log(pokeID)
-// returns a number between 1-898
-// --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+function getMon() {
+const pokeID = Math.floor((Math.random()*898) +1)
+axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeID}/`).then(json => {
+    type = json.data.types[0].type.url;
+    axios.get(type).then(json2 => {
+        let pokeList = json2.data.pokemon;
+        const pokemon_containers = document.querySelectorAll("#pokemon_container")
+        for (const container of pokemon_containers) {
+            const pokemon_name = container.querySelector(".poke_name")
+            const pokemon_image = container.querySelector("#pokeball")
+            random_poke = Math.floor(Math.random() * pokeList.length)
+            pokemon_name.innerHTML = pokeList[random_poke].pokemon.name;
+            axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeList[random_poke].pokemon.name}/`).then(response => {
+                pokemon_image.src = response.data.sprites.front_default
+            })
+        }
+    })
+}).catch(error => {
+    console.error("error in request", error)
+    pokemon_name.innerHTML = "an error has occupied"
+    pokemon_image.src = ""
+})
 
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-// Each elemental Type button
-// const types = ["fire", "water", "electric", "grass", "ice", "ground", "flying", "ghost", "rock", "fighting", "poison", "psychic", "bug", "dark", "steel", "dragon", "fairy", "normal"]
-// let randomType = types[Math.trunc(Math.random() * types.length)];
-// console.log(randomType)
-// // returns each type
-
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-// const getMon = async ()=> {
-//     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeID}/`)   
-//     const pokeName = response.data.name
-//     const pokeType = response.data.types[0].type.url
-
-// }
-
+}
